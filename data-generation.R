933908b9d11ed77b9a83320da1ccf745831ecdca
Georgios Giannarakis <georgiosgiannarakis@gmail.com>
2024-01-02 19:50:45 +0200

    final dataset, app working locally with polars

--- data-generation.R
+++ data-generation.R
@@ -1,25 +1,14 @@
 install.packages("pwr")
 library(pwr)
 
-n_c = 5000
-n_t = 5000
-total = n_c + n_t
-bal = max(n_c, n_t) / min(n_c, n_t)
-bal_str = paste(toString(max(n_c, n_t) / min(n_c, n_t)), "1", sep =":")
-p_c = 0.15
-p_t = 0.16
-mde_abs = abs(p_t - p_c)
-mde_rel = abs((p_t - p_c) / p_c)
-h = abs(2*asin(sqrt(p_c))-2*asin(sqrt(p_t)))
-alpha = 0.10
-pow = pwr.2p2n.test(h, n1=n_c, n2=n_t, sig.level=alpha)$power
-
+# Define the functions that computes power of a design
 calculate_power <- function(n_c, n_t, p_c, p_t, alpha) {
+  
   total <- n_c + n_t
-  bal <- max(n_c, n_t) / min(n_c, n_t)
-  bal_str <- paste(toString(max(n_c, n_t) / min(n_c, n_t)), "1", sep = ":")
-  mde_abs <- abs(p_t - p_c)
-  mde_rel <- abs((p_t - p_c) / p_c)
+  #bal <- max(n_c, n_t) / min(n_c, n_t)
+  #bal_str <- paste(toString(max(n_c, n_t) / min(n_c, n_t)), "1", sep = ":")
+  #mde_abs <- abs(p_t - p_c)
+  #mde_rel <- abs((p_t - p_c) / p_c)
   h <- abs(2 * asin(sqrt(p_c)) - 2 * asin(sqrt(p_t)))
   
   pow <- pwr.2p2n.test(h, n1 = n_c, n2 = n_t, sig.level = alpha)$power
@@ -27,28 +16,36 @@ calculate_power <- function(n_c, n_t, p_c, p_t, alpha) {
   return(pow)
 }
 
-# Create an empty data frame to store the results
-results_df <- data.frame()
+# Parameter ranges and step sizes
+n_c_values <- c(seq(100, 1000, by = 50), 
+                seq(1000, 5000, by = 100), 
+                seq(5000, 25000, by = 500), 
+                seq(25000, 50000, by = 1000))
+imbalance <- c(1, 3, 5, 7, 9)
+p_c_values <- seq(0.01, 0.50, by = 0.01)
+p_t_values <- seq(0.01, 0.50, by = 0.01)
+alpha_values <- c(0.01, 0.05, 0.10, 0.15, 0.20)
 
-# Define the range of values for each parameter
-n_c_values <- c(500, 1000, 2000)
-n_t_values <- c(500, 1000, 2000)
-p_c_values <- c(0.1, 0.2, 0.3)
-p_t_values <- c(0.15, 0.25, 0.35)
-alpha_values <- c(0.05, 0.1, 0.15)
+# Initialize empty list
+results_list <- list()
 
 # Nested loop to iterate over all combinations of parameters
 for (n_c in n_c_values) {
-  for (n_t in n_t_values) {
+  for (i in imbalance) {
     for (p_c in p_c_values) {
       for (p_t in p_t_values) {
         for (alpha in alpha_values) {
+          
+          # the size of the treatment group is a multiple of the control group
+          n_t = n_c*i
+          
           # Call the function to calculate power
           pow <- calculate_power(n_c, n_t, p_c, p_t, alpha)
           
           # Create a data frame with the results and input parameters
           result_entry <- data.frame(
             n_c = n_c,
+            i = i,
             n_t = n_t,
             p_c = p_c,
             p_t = p_t,
@@ -56,13 +53,17 @@ for (n_c in n_c_values) {
             power = pow
           )
           
-          # Append the result to the main data frame
-          results_df <- rbind(results_df, result_entry)
+          # Append it to the result's list
+          results_list[[length(results_list) + 1]] <- result_entry
+          
         }
       }
     }
   }
 }
 
+# Combine all data frames
+results <- do.call(rbind, results_list)
+
 # Export the data frame to a CSV file
-write.csv(results_df, "power_results.csv", row.names = TRUE)
+write.csv(results, "power_results_all.csv", row.names = TRUE)
--- main.py
+++ main.py
@@ -1,43 +1,61 @@
 import streamlit as st
 import pandas as pd
+import polars as pl
+import numpy as np
 
-# Load the CSV file
-df = pd.read_csv("power_results.csv")
+# # Using Polars:
+df = pl.read_csv("power_results_all.csv", ignore_errors=True)
 
 # Set the page title and description
 st.title("Statistical Power Calculator")
 st.subheader("Calculate statistical power for different parameter values")
 
-# Create sliders for each column (except "power") with a specified step
-n_c_slider = st.slider("Number of samples in Control group (n_c)", min_value=int(df["n_c"].min()), max_value=int(df["n_c"].max()), value=int(df["n_c"].mean()), step=100)
-n_t_slider = st.slider("Number of samples in Treatment group (n_t)", min_value=int(df["n_t"].min()), max_value=int(df["n_t"].max()), value=int(df["n_t"].mean()), step=100)
+# Create the control size slider
+control_array = np.concatenate([
+    np.arange(100, 1001, 50),
+    np.arange(1000, 5001, 100),
+    np.arange(5000, 25001, 500),
+    np.arange(25000, 50001, 1000)
+])
+n_c_slider = st.select_slider("Select control group size", options=control_array)
+
+# Create the multiplier slider
+multiplier_options = {'x1': 1, 'x3': 3, 'x5': 5, 'x7': 7, 'x9': 9}
+n_t_slider = st.select_slider("Select imbalance", options=list(multiplier_options.keys()))
+selected_value = multiplier_options[n_t_slider]
+
+# Calculate the treatment group size
+result = n_c_slider * multiplier_options[n_t_slider]
+st.write(f"Control group size is: {n_c_slider}. Treatment group size is: {result}")
 
 # Allow manual input for p_c and p_t in a horizontal layout
 col1, col2 = st.columns(2)
 with col1:
-    p_c_manual = st.text_input("Probability of success in Control group (p_c)", df["p_c"].mean())
+    p_c_manual = st.text_input("Probability of success in Control group (p_c)", 0.01)
 
 with col2:
-    p_t_manual = st.text_input("Probability of success in Treatment group (p_t)", df["p_t"].mean())
+    p_t_manual = st.text_input("Probability of success in Treatment group (p_t)", 0.02)
 
 # Convert input values to float
-p_c_manual = float(p_c_manual) if p_c_manual else df["p_c"].mean()
-p_t_manual = float(p_t_manual) if p_t_manual else df["p_t"].mean()
+p_c_manual = float(p_c_manual) if p_c_manual else df["p_c"].min()
+p_t_manual = float(p_t_manual) if p_t_manual else df["p_t"].min()
 
-alpha_slider = st.slider("Significance Level (alpha)", min_value=df["alpha"].min(), max_value=df["alpha"].max(), value=df["alpha"].mean(), step=0.01)
+# Create the alpha slider
+alpha_array = [0.01, 0.05, 0.10, 0.15, 0.20]
+alpha_slider = st.select_slider("Significance Level (alpha)", options=alpha_array)
 
-# Filter the dataframe based on slider and manual input values
-filtered_df = df[
-    (df["n_c"] == n_c_slider) &
-    (df["n_t"] == n_t_slider) &
-    (df["p_c"] == p_c_manual) &
-    (df["p_t"] == p_t_manual) &
-    (df["alpha"] == alpha_slider)
-]
+# Filter the DataFrame using Polars
+filtered_df = df.filter(
+    (pl.col("n_c") == n_c_slider) &
+    (pl.col("i") == selected_value) &
+    (pl.col("p_c") == p_c_manual) &
+    (pl.col("p_t") == p_t_manual) &
+    (pl.col("alpha") == alpha_slider)
+)
 
 # Display the corresponding power value with increased font size and centered
-if not filtered_df.empty:
-    power_value = filtered_df["power"].iloc[0]
-    st.markdown(f'<div style="font-size:24px; text-align:center;">The <strong>statistical power</strong> for the given parameters is: <strong>{power_value:.4f}</strong></div>', unsafe_allow_html=True)
+if not filtered_df.is_empty():
+    power_value = filtered_df[0]["power"][0]
+    st.markdown(f'<div style="font-size:24px; text-align:center;">The <strong>statistical power</strong> for the given parameters is: <strong>{power_value:.2f}</strong></div>', unsafe_allow_html=True)
 else:
     st.warning("No matching data found for the selected parameters.")
